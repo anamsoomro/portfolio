fa34ee834bba971b75d3bf09a66756802fb7c8b1
anamsoomro <anamsoomroed@gmail.com>
2020-05-16 00:25:01 -0500

    Updates

---
+++ precache-manifest.2c7198f637019e19d0bc9546af552171.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd094031ab88395651a8ef97305e18c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eee6539466d9f3b631f3",
+    "url": "/static/css/main.a72f6b75.chunk.css"
+  },
+  {
+    "revision": "3362ef8b16fbf85dd4ea",
+    "url": "/static/js/2.a97e36d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a97e36d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee6539466d9f3b631f3",
+    "url": "/static/js/main.a43d0a5e.chunk.js"
+  },
+  {
+    "revision": "772ed9dfdac89fc24126",
+    "url": "/static/js/runtime-main.45c05def.js"
+  },
+  {
+    "revision": "c08ca09ca8b37e076ba3f41d2a1d7afc",
+    "url": "/static/media/Soomro_Resume.c08ca09c.pdf"
+  },
+  {
+    "revision": "82e1acd156ecb833fa2dec38869fc3ca",
+    "url": "/static/media/atomicjob.82e1acd1.gif"
+  },
+  {
+    "revision": "a95c4798bbdb3352b8a798c5e9c744df",
+    "url": "/static/media/atomicjob.a95c4798.png"
+  },
+  {
+    "revision": "142262900c729ba6c6bbc5be419526cb",
+    "url": "/static/media/captionit.14226290.png"
+  },
+  {
+    "revision": "407a2144c2ff0b7ea7b214c34cbce539",
+    "url": "/static/media/captionit.407a2144.gif"
+  },
+  {
+    "revision": "63161b71e77c9aba9c4e331e7f3d0085",
+    "url": "/static/media/moneymoon.63161b71.gif"
+  },
+  {
+    "revision": "ea6501a852717695a9510e8351e9bd6a",
+    "url": "/static/media/moneymoon1.ea6501a8.png"
+  },
+  {
+    "revision": "b61129fe0bdb48b8769b59cefbba8c97",
+    "url": "/static/media/moneymoon2.b61129fe.png"
+  },
+  {
+    "revision": "34fa607be95b936e255a7b672cce7808",
+    "url": "/static/media/moneymoon3.34fa607b.png"
+  },
+  {
+    "revision": "5668ef8ce58f320063656c2ec0b09398",
+    "url": "/static/media/moneymoon4.5668ef8c.png"
+  },
+  {
+    "revision": "ba2a3a9876f026da4e9245b95527bd61",
+    "url": "/static/media/moneymoon5.ba2a3a98.png"
+  },
+  {
+    "revision": "067e90d438bb2555c81da0abd7e01677",
+    "url": "/static/media/pictureshuffle.067e90d4.png"
+  },
+  {
+    "revision": "0e9d140d6296c32e61ba3eaf44fdda74",
+    "url": "/static/media/pictureshuffle.0e9d140d.gif"
+  },
+  {
+    "revision": "79acd9960ed50f1ac39fc4b8321477f3",
+    "url": "/static/media/typewars.79acd996.gif"
+  },
+  {
+    "revision": "da1fb853f8f4a67813512afe2f299271",
+    "url": "/static/media/typewars.da1fb853.png"
+  }
+]);
